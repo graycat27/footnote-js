8f0cffb0286b62edea3d41fcc36a074c43d40f1c
graycat27 <64903817+graycat27@users.noreply.github.com>
2022-02-06 12:38:12 +0900

    fix #2

--- src/js/footnote.js
+++ src/js/footnote.js
@@ -116,10 +116,10 @@ let footnoteJs = (function () {
         fn: fn
     };
 }());
+footnoteJs.fn.init();
 footnoteJs.fn.registerCustomTagFootNote();
 
 window.addEventListener('DOMContentLoaded', function(){
-    footnoteJs.fn.init();
     footnoteJs.fn.rescueMissedFootNote();
 });
 
